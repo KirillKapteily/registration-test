35e73dac58adcc3046c2dd06f16a88723813c390
Кирилл Каптелий <161931268+KirillKapteily@users.noreply.github.com>
2025-02-15 11:08:02 +0200

    add tes
    test

---
+++ index.js
@@ -0,0 +1,12 @@
+const userName = prompt("What's your name?");
+console.log(userName)
+const userSurename = prompt("What's your Surname?");
+console.log(userSurename)
+let userTown = prompt("What's your town?");
+console.log(userTown)
+let userEmail = prompt("What's your Email?");
+console.log(userEmail)
+let userPass = prompt("What's your Password?");
+console.log(userPass)
+
+
